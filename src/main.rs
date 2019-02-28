8347801e781d6cb3f8074e0c2cacc15a16d032d5
Kied T L <eli.georgios@gmail.com>
2019-02-28 08:30:13 -0500

    add main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn rand_parkmiller(state: u64) -> u64 {
     return (state * 48271) % 0x7fffffff;
 }
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let arg = &args[1];
+    let state: u64 = arg.parse::<u64>().unwrap();
+    println!("{}", rand_parkmiller(state));
 }
